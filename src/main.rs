9532420b1aaa737e74e9a87887f8df0f1081a0f6
jeeva <jeevaprakashdr@gmail.com>
2026-03-31 15:20:00 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,38 @@
 #![allow(unused_imports)]
-use std::{io::Write, net::TcpListener};
+use std::{io::{Read, Write}, net::TcpListener, thread};
+
+use bytes::buf;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment the code below to pass the first stage
-    
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
     for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-                let _ = stream.write(b"+PONG\r\n");
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
+    }
+}
+
+fn handle_connection(stream: Result<std::net::TcpStream, std::io::Error>) {
+    match stream {
+        Ok(mut stream) => {
+            println!("accepted new connection");
+            loop {
+                let mut buffer = [0; 512];
+                let  bytes_count = stream.read(&mut buffer[..]).unwrap();
+                
+                if bytes_count == 0 {
+                    break;
+                }
+
+                stream.write(b"+PONG\r\n").unwrap();
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+        }
+        Err(e) => {
+            println!("error: {}", e);
         }
     }
 }
