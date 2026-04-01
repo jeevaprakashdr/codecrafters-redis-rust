a273aa396a51a7f13b475728a2b557157f6090be
jeeva <jeevaprakashdr@gmail.com>
2026-04-01 18:59:27 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![allow(unused_imports)]
-use std::{io::{Read, Write}, net::TcpListener, thread};
+use std::{collections::HashSet, io::{Read, Write}, net::TcpListener, str, thread};
 
 use bytes::buf;
 
@@ -27,8 +27,37 @@ fn handle_connection(stream: Result<std::net::TcpStream, std::io::Error>) {
                 if bytes_count == 0 {
                     break;
                 }
-
-                stream.write(b"+PONG\r\n").unwrap();
+                
+                let command_arr :Vec<&str> = str::from_utf8(&buffer[..bytes_count])
+                    .unwrap()
+                    .trim()
+                    .split("\r\n")
+                    .collect();
+                
+                for argument in command_arr {
+                    match argument.chars().next() {
+                        Some('*') => {
+                            // Handle array marker
+                        }
+                        Some('$') => {
+                            // Handle bulk string marker
+                        }
+                        _ => {
+                            match argument.to_lowercase().as_str() {
+                                "echo" => {}
+                                "ping" => {
+                                    println!("found");
+                                    stream.write(b"+PONG\r\n").unwrap();
+                                }
+                                _ => {
+                                    let val = format!("${}\r\n{}\r\n",argument.len(), argument);
+                                    stream.write(val.as_bytes()).unwrap();
+                                }
+                            }
+                            
+                        }
+                    }
+                }
             }
         }
         Err(e) => {
