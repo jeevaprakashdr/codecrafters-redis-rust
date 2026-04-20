860541d4a7c42bc32c874e05fd755db86b61898d
jeeva <jeevaprakashdr@gmail.com>
2026-04-20 14:25:07 +0200

    codecrafters submit [skip ci]

--- src/redis/command/xread.rs
+++ src/redis/command/xread.rs
@@ -16,32 +16,48 @@ impl Command for Xread {
 
         let default  = StreamEntryId { ms: 0, seqno: 0 };
         let start = StreamEntryId::from_str(self.args[3].as_str()).unwrap_or(default);
-        match db.get_mut(self.args[2].to_string()) {
-            Some(data) => {
-                let filtered: Vec<&Stream> = data.stream
-                    .iter()
-                    .filter(|stream| stream.id >= start)
-                    .collect::<Vec<_>>();
+        let stream_keys = self.args.iter().skip(2).filter(|f| !f.contains("-")).collect::<Vec<_>>();
 
-                let stream_data : Vec<String> = filtered
-                    .iter()
-                    .map(|stream| {
-                        let mut streams = Vec::<String>::new();
-                        streams.push(create_bulk_string (stream.id.to_string().as_str()));
-                        streams.push(create_array(&stream.entries.iter().map(|val| val.as_str()).collect::<Vec<_>>()));
-                        streams
-                    })
-                    .map(|streams|  format!("*{}\r\n{}", streams.len(), streams.join("")))
-                    .collect();                    
-                    
-                let mut out = Vec::<String>::new();
-                out.push(create_bulk_string(self.args[2].as_str()));
-                out.push(format!("*{}\r\n{}", stream_data.len(), stream_data.join("").to_string()));
+        let out: Vec<String> = stream_keys
+            .iter()
+            .map(|key|fetch_data(&mut db, start, key))
+            .map(|stream_data| format!("*{}\r\n{}", stream_data.len(), stream_data.join("").to_string()))
+            .collect();
 
-                Ok(format!("*1\r\n*{}\r\n{}", out.len(), out.join("").to_string()))
-            },
-            None => Ok(create_empty_array()),
+        if out.len() > 0 {
+            Ok(format!("*{}\r\n{}", out.len(), out.join("").to_string()))
+        } else {
+            Ok(create_empty_array())
         }
     }
+}
+
+fn fetch_data(db: &mut std::sync::MutexGuard<'_, db::InMemoryDb>, start: StreamEntryId, stream_key: &String) -> Vec<String> {
+    match db.get_mut(stream_key.to_string()) {
+        Some(data) => {
+
+            let filtered: Vec<&Stream> = data.stream
+                .iter()
+                .filter(|stream| stream.id >= start)
+                .collect::<Vec<_>>();
+
+            let stream_data : Vec<String> = filtered
+                .iter()
+                .map(|stream| {
+                    let mut streams = Vec::<String>::new();
+                    streams.push(create_bulk_string (stream.id.to_string().as_str()));
+                    streams.push(create_array(&stream.entries.iter().map(|val| val.as_str()).collect::<Vec<_>>()));
+                    streams
+                })
+                .map(|streams|  format!("*{}\r\n{}", streams.len(), streams.join("")))
+                .collect();                    
+            
+            let mut data = Vec::new();
+            data.push(create_bulk_string(stream_key.as_str()));
+            data.push(format!("*{}\r\n{}", stream_data.len(), stream_data.join("").to_string()));
+            data
+        },
+        None => { vec![] },
+    }    
 }
 
