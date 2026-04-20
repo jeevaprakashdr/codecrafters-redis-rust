4a5546af5104982e03516cb58b6c793e12e920eb
jeeva <jeevaprakashdr@gmail.com>
2026-04-21 00:57:16 +0200

    codecrafters submit [skip ci]

--- src/redis/command/xread.rs
+++ src/redis/command/xread.rs
@@ -1,5 +1,4 @@
-use core::time;
-use std::thread;
+use std::{thread, time};
 use std::time::Duration;
 use std::{str::FromStr, sync::Arc};
 
@@ -14,33 +13,35 @@ pub struct Xread {
 
 impl Command for Xread {
     fn execute (&self) -> Result<String, &'static str> {
-        let in_memory_db = Arc::clone(&DB);
-        let mut db: std::sync::MutexGuard<'_, db::InMemoryDb> = in_memory_db.lock().unwrap();
-
         let blocking_request = self.args.iter().any(|arg| arg.to_lowercase() == "block".to_string());
         let (args_skip_index, timeout) = 
-            if blocking_request { (4, u64::from_str(self.args[3].as_str()).unwrap_or(0)) } else { (2, 0) };
-
+            if blocking_request { (4, u64::from_str(self.args[2].as_str()).unwrap_or(0)) } else { (2, 0) };
+        println!("timeout {}", timeout);
         let mut timeout_expired = false;
         loop {
-            if let Ok(data) = self.execute(&mut db, args_skip_index) {
+            if let Ok(data) = self.execute(args_skip_index) {
                 return Ok(data)
             }
-            println!("no data found");
+            
             if timeout_expired {
+                println!("expired");
                 return Ok(create_null_array())
             }
-            
+
             if timeout > 0 {
-                thread::sleep(Duration::from_millis(timeout));
-                timeout_expired = true;
+                println!("blocked");
+                thread::sleep(time::Duration::from_secs(1));
+                timeout_expired = true
             }
         }    
     }
 }
 
 impl Xread {
-    fn execute(&self, mut db: &mut std::sync::MutexGuard<'_, db::InMemoryDb>, args_skip_index: usize) -> Result<String, &'static str> {
+    fn execute(&self, args_skip_index: usize) -> Result<String, &'static str> {
+        let in_memory_db = Arc::clone(&DB);
+        let mut db: std::sync::MutexGuard<'_, db::InMemoryDb> = in_memory_db.lock().unwrap();
+        
         let default  = StreamEntryId { ms: 0, seqno: 0 };
         let stream_keys = self.args.iter().skip(args_skip_index).filter(|f| !f.contains("-")).collect::<Vec<_>>();
         let start_entry_ids = self.args
@@ -48,7 +49,7 @@ impl Xread {
             .skip(args_skip_index)
             .filter(|f| f.contains("-")).map(|f| StreamEntryId::from_str(f.as_str()).unwrap_or(default))
             .collect::<Vec<_>>();
-        println!("stream-keys {:?} ---- entry-ids {:?}", stream_keys, start_entry_ids);
+        
         let out: Vec<String> = stream_keys
             .iter()
             .enumerate()
@@ -56,7 +57,6 @@ impl Xread {
             .filter(|vec| !vec.is_empty())
             .map(|stream_data| format!("*{}\r\n{}", stream_data.len(), stream_data.join("")))
             .collect();
-
         if !out.is_empty() {
             Ok(format!("*{}\r\n{}", out.len(), out.join("")))
         } else {
